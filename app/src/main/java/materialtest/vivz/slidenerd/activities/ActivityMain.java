9aea3f29cd52e75c0b1a23e494178814ecbef7f0
Tony Baby <tonybabymannath@gmail.com>
2016-06-21 21:23:07 +0530

    Initial commit

---
+++ app/src/main/java/materialtest/vivz/slidenerd/activities/ActivityMain.java
@@ -0,0 +1,269 @@
+package materialtest.vivz.slidenerd.activities;
+
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+import materialtest.vivz.slidenerd.anim.AnimationUtils;
+import materialtest.vivz.slidenerd.extras.SortListener;
+import materialtest.vivz.slidenerd.fragments.FragmentBoxOffice;
+import materialtest.vivz.slidenerd.fragments.FragmentDrawer;
+import materialtest.vivz.slidenerd.fragments.FragmentSearch;
+import materialtest.vivz.slidenerd.fragments.FragmentUpcoming;
+import materialtest.vivz.slidenerd.logging.L;
+import materialtest.vivz.slidenerd.materialtest.R;
+import materialtest.vivz.slidenerd.services.ServiceMoviesBoxOffice;
+import me.tatarka.support.job.JobInfo;
+import me.tatarka.support.job.JobScheduler;
+
+
+public class ActivityMain extends ActionBarActivity implements MaterialTabListener {
+
+    //int representing our 0th tab corresponding to the Fragment where search results are dispalyed
+    public static final int TAB_SEARCH_RESULTS = 0;
+    //int corresponding to our 1st tab corresponding to the Fragment where box office hits are dispalyed
+    public static final int TAB_HITS = 1;
+    //int corresponding to our 2nd tab corresponding to the Fragment where upcoming movies are displayed
+    public static final int TAB_UPCOMING = 2;
+    //int corresponding to the number of tabs in our Activity
+    public static final int TAB_COUNT = 3;
+    //int corresponding to the id of our JobSchedulerService
+    private static final int JOB_ID = 100;
+
+    //Run the JobSchedulerService every 2 minutes
+    private static final long POLL_FREQUENCY = 28800000;
+    private JobScheduler mJobScheduler;
+    private Toolbar mToolbar;
+    //a layout grouping the toolbar and the tabs together
+    private ViewGroup mContainerToolbar;
+    private MaterialTabHost mTabHost;
+    private ViewPager mPager;
+    private ViewPagerAdapter mAdapter;
+    private FloatingActionButton mFAB;
+    private FloatingActionMenu mFABMenu;
+    private FragmentDrawer mDrawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupTabs();
+        setupJob();
+        setupDrawer();
+        //animate the Toolbar when it comes into the picture
+        AnimationUtils.animateToolbarDroppingDown(mContainerToolbar);
+
+    }
+
+    private void setupDrawer() {
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        mContainerToolbar = (ViewGroup) findViewById(R.id.container_app_bar);
+        //set the Toolbar as ActionBar
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //setup the NavigationDrawer
+        mDrawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        mDrawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+    }
+
+
+    public void onDrawerItemClicked(int index) {
+        mPager.setCurrentItem(index);
+    }
+
+    public View getContainerToolbar() {
+        return mContainerToolbar;
+    }
+
+    private void setupTabs() {
+        mTabHost = (MaterialTabHost) findViewById(R.id.materialTabHost);
+        mPager = (ViewPager) findViewById(R.id.viewPager);
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mAdapter);
+        //when the page changes in the ViewPager, update the Tabs accordingly
+        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                mTabHost.setSelectedNavigationItem(position);
+
+            }
+        });
+        //Add all the Tabs to the TabHost
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            mTabHost.addTab(
+                    mTabHost.newTab()
+                            .setIcon(mAdapter.getIcon(i))
+                            .setTabListener(this));
+        }
+    }
+
+    private void setupJob() {
+        mJobScheduler = JobScheduler.getInstance(this);
+        //set an initial delay with a Handler so that the data loading by the JobScheduler does not clash with the loading inside the Fragment
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //schedule the job after the delay has been elapsed
+                buildJob();
+            }
+        }, 30000);
+    }
+
+    private void buildJob() {
+        //attach the job ID and the name of the Service that will work in the background
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, new ComponentName(this, ServiceMoviesBoxOffice.class));
+        //set periodic polling that needs net connection and works across device reboots
+        builder.setPeriodic(POLL_FREQUENCY)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true);
+        mJobScheduler.schedule(builder.build());
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present. 
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will 
+        // automatically handle clicks on the Home/Up button, so long 
+        // as you specify a parent activity in AndroidManifest.xml. 
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement 
+        if (id == R.id.action_settings) {
+            L.m("Settings selected");
+            return true;
+        }
+        if (id == R.id.action_touch_intercept_activity) {
+            startActivity(new Intent(this, ActivityTouchEvent.class));
+        }
+
+        if (R.id.action_activity_calling == id) {
+            startActivity(new Intent(this, ActivityA.class));
+        }
+        if (R.id.action_shared_transitions == id) {
+            startActivity(new Intent(this, ActivitySharedA.class));
+        }
+        if (R.id.action_tabs_using_library == id) {
+            startActivity(new Intent(this, ActivitySlidingTabLayout.class));
+        }
+        if (R.id.action_vector_test_activity == id) {
+            startActivity(new Intent(this, ActivityVectorDrawable.class));
+        }
+
+        if (R.id.action_dynamic_tabs_activity == id) {
+            startActivity(new Intent(this, ActivityDynamicTabs.class));
+        }
+        if (R.id.action_recycler_item_animations == id) {
+            startActivity(new Intent(this, ActivityRecylerAnimators.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+        //when a Tab is selected, update the ViewPager to reflect the changes
+        mPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+    }
+
+
+
+
+    private void toggleTranslateFAB(float slideOffset) {
+        if (mFABMenu != null) {
+            if (mFABMenu.isOpen()) {
+                mFABMenu.close(true);
+            }
+            mFAB.setTranslationX(slideOffset * 200);
+        }
+    }
+
+    public void onDrawerSlide(float slideOffset) {
+        toggleTranslateFAB(slideOffset);
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        int icons[] = {R.drawable.ic_action_search,
+                R.drawable.ic_action_trending,
+                R.drawable.ic_action_upcoming};
+
+        FragmentManager fragmentManager;
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragmentManager = fm;
+        }
+
+        public Fragment getItem(int num) {
+            Fragment fragment = null;
+//            L.m("getItem called for " + num);
+            switch (num) {
+                case TAB_SEARCH_RESULTS:
+                    fragment = FragmentSearch.newInstance("", "");
+                    break;
+                case TAB_HITS:
+                    fragment = FragmentBoxOffice.newInstance("", "");
+                    break;
+                case TAB_UPCOMING:
+                    fragment = FragmentUpcoming.newInstance("", "");
+                    break;
+            }
+            return fragment;
+
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getStringArray(R.array.tabs)[position];
+        }
+
+        private Drawable getIcon(int position) {
+            return getResources().getDrawable(icons[position]);
+        }
+    }
+} 
